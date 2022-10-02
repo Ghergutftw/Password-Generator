8de9c856e24c37f739d7b691637de4d2383d4586
GherMa <89106572+Ghergutftw@users.noreply.github.com>
2022-10-02 11:08:38 +0300

    Update PassWord Generator.py

--- PassWord Generator.py
+++ PassWord Generator.py
@@ -8,17 +8,17 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+nrLetters = int(input("How many letters would you like in your password?\n"))
+nrSymbols = int(input(f"How many symbols would you like?\n"))
+nrNumbers = int(input(f"How many numbers would you like?\n"))
 
 
 passwordList = []
-for char in range(1, nr_letters + 1):
+for char in range(1, nrLetters + 1):
     passwordList.append(random.choice(letters))
-for char in range(1, nr_symbols + 1):
+for char in range(1, nrSymbols + 1):
     passwordList.append(random.choice(symbols))
-for char in range(1, nr_numbers + 1):
+for char in range(1, nrNumbers + 1):
     passwordList.append(random.choice(numbers))
 
 print(passwordList)
